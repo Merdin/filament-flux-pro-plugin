2e8525d73ed0d9c1aa6646324247d3325d87274f
Merdin Kahrimanović <me@merdin.dev>
2026-05-14 13:57:19 +0200

    refactor: Type to self

--- src/Components/DatePicker/Enums/Type.php
+++ src/Components/DatePicker/Enums/Type.php
@@ -9,7 +9,7 @@ enum Type: string
     case button = 'button';
     case input = 'input';
 
-    public static function default(): Type
+    public static function default(): self
     {
         return self::button;
     }
