09b6de5b8387f49c01d09e924d591427fed2c068
Merdin Kahrimanović <me@merdin.dev>
2026-05-16 01:09:57 +0200

    remove unneeded build.js

--- resources/views/components/date-picker/date-picker.blade.php
+++ resources/views/components/date-picker/date-picker.blade.php
@@ -1,28 +1,33 @@
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
 
     <div x-data="{ state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }} }">
-        <x-flux::date-picker x-model="state" @if ($getType()) type="{{ $getType() }}" @endif
-            @if ($getMode()) mode="{{ $getMode() }}" @endif
-            @if ($getWeekNumbers()) week-numbers @endif @if ($getFixedWeeks()) fixed-weeks @endif
-            @if ($getWithToday()) with-today @endif
-            @if ($getSelectableHeader()) selectable-header @endif
-            @if (filled($getOpenTo())) open-to="{{ $getOpenTo() }}" @endif
-            @if ($getForceOpenTo()) force-open-to @endif
-            @if (filled($getMin())) min="{{ $getMin() }}" @endif
-            @if (filled($getMax())) max="{{ $getMax() }}" @endif
-            @if (filled($getUnavailable())) unavailable="{{ $getUnavailable() }}" @endif
-            @if (filled($getSize())) size="{{ $getSize() }}" @endif
-            @if (filled($getStartDay())) start-day="{{ $getStartDay() }}" @endif
-            @if ($getWithInputs()) with-inputs="{{ $getWithInputs() }}" @endif
-            @if ($getWithConfirmation()) with-confirmation @endif
-            @if ($getWithPresets()) with-presets @endif
-            @if ($getMonths()) months="{{ $getMonths() }}" @endif
-            @if ($getWithPresets()) presets="{{ $getPresets() }}" @endif
-            @if ($getClearable()) clearable @endif @if ($isDisabled()) disabled @endif
-            @if ($getInvalid()) invalid @endif
-            @if (filled($getLocale())) locale="{{ $getLocale() }}" @endif
-            @if (filled($getMinRange())) min-range="{{ $getMinRange() }}" @endif
-            @if (filled($getMaxRange())) max-range="{{ $getMaxRange() }}" @endif />
+        <x-flux::date-picker
+            x-model="state"
+            :type="$getType()"
+            :mode="$getMode()"
+            :week-numbers="$getWeekNumbers() ?: null"
+            :fixed-weeks="$getFixedWeeks() ?: null"
+            :with-today="$getWithToday() ?: null"
+            :selectable-header="$getSelectableHeader() ?: null"
+            open-to="{{ $getOpenTo() }}"
+            :force-open-to="$getForceOpenTo() ?: null"
+            :min="$getMin()"
+            :max="$getMax()"
+            :unavailable="$getUnavailable()"
+            :size="$getSize()"
+            :start-day="$getStartDay()"
+            :with-inputs="$getWithInputs()"
+            :with-confirmation="$getWithConfirmation() ?: null"
+            :with-presets="$getWithPresets() ?: null"
+            :months="$getMonths()"
+            :presets="$getWithPresets() ? $getPresets() : null"
+            :clearable="$getClearable() ?: null"
+            :disabled="$isDisabled() ?: null"
+            :invalid="$getInvalid() ?: null"
+            locale="{{ $getLocale() }}"
+            :min-range="$getMinRange()"
+            :max-range="$getMaxRange()"
+        />
     </div>
 
 </x-dynamic-component>
